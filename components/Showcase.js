8f117b42fd5ef17935857f2fa62714825caf83ec
Dilano Bo <dilano.bo@protonmail.com>
2022-11-15 15:40:42 -0800

    fix: html bg height

--- components/Showcase.js
+++ components/Showcase.js
@@ -63,7 +63,7 @@ const Showcase = () => {
 
   return (
     <div className="flex flex-col justify-around md:flex-row">
-      <div className="showcase-left pt-3 leading-medium-loose self-end">
+      <div className="showcase-left self-end pt-3 leading-medium-loose">
         <h1 className="max-w-md text-center font-Do-Hyeon text-xl5 sm:text-left">
           Invest by Watching Your Favorite Content
         </h1>
@@ -83,7 +83,7 @@ const Showcase = () => {
           <button
             type="submit"
             onClick={handleSubmit}
-            className="mt-2 w-full rounded-xl border-b-4 border-2 border-black bg-white p-4 font-ProtoMono-SemiBold hover:border-b-2 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-none"
+            className="mt-2 w-full rounded-xl border-2 border-b-4 border-black bg-white p-4 font-ProtoMono-SemiBold hover:border-b-2 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-none"
           >
             Stay Updated
           </button>
@@ -111,14 +111,13 @@ const Showcase = () => {
         </div>
       </div>
       <div className="showcase-right self-start">
-      
         <Image
           alt="Video"
           src="/images/right-group.svg"
           width={650}
           height={650}
           priority
-          className="mx-3 block pt-3"
+          className="mx-3 block pt-3 pr-6"
         />
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import Showcase from "../components/Showcase";
 
 export default function Home() {
   return (
-    <div className="w-[100vw] v-[101vh]">
+    <div className="w-[100vw]">
       <Showcase />
     </div>
   );
 }
-
