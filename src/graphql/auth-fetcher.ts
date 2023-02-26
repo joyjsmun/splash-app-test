67f5aab8f839d9149d90d35451e293a51153e1e3
joyjsmun <joy.js.mun@gmail.com>
2023-02-25 22:42:50 -0700

    ADD: x-access-token

--- src/graphql/auth-fetcher.ts
+++ src/graphql/auth-fetcher.ts
@@ -1,3 +1,6 @@
+import { isTokenExpired, readAccessToken } from "src/lib/auth/helpers";
+import refreshAccessToken from "src/lib/auth/refreshAccessToken";
+
 // Lens graphql server endpoint
 const endpoint = "https://api-mumbai.lens.dev";
 export const fetcher = <TData, TVariables>(
@@ -5,13 +8,39 @@ export const fetcher = <TData, TVariables>(
   variables?: TVariables,
   options?: RequestInit['headers']
 ): (() => Promise<TData>) => {
+
+  async function getAccessToken(){
+    const token = readAccessToken();
+
+    if(!token){
+      return null;
+    }
+
+    let accessToken = token.accessToken;
+
+    if (isTokenExpired(token.exp)){
+      const newToken = await refreshAccessToken();
+
+      if(!newToken) return null;
+
+      accessToken = newToken;
+    }
+
+    return accessToken;
+  }
+
+
   return async () => {
+
+    const token = typeof window!== "undefined" ? await getAccessToken(): null;
     const res = await fetch(endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         ...options,
         //authentication headers
+        "x-access-token": token ? token : "",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
         query,
